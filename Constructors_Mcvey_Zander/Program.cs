5a66f9c9d16b6233d4ec911b10bd6ddde9398300
Zander <114107976+Femnyx@users.noreply.github.com>
2023-04-25 14:29:23 -0500

    Comments I have added!

--- Constructors_Mcvey_Zander/Program.cs
+++ Constructors_Mcvey_Zander/Program.cs
@@ -6,16 +6,16 @@ namespace Constructors_Mcvey_Zander
     {
         static void Main(string[] args)
         {
-            //
+            //This states myGame saves the orange text under that name.
             game myGame = new game("Legend of Zelda: WindWaker", "Adventure");
             
-            //
+            //This line prints out what their favorite game is with myGame.title and myGame.genre.
             Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre} game!");
 
-            //
+            //This makes another variable to list their other favorite game
             game myOtherGame = new game();
 
-            //
+            //This line prints out what their other favorite game is with myOtherGame.title and myOtherGame.genre.
             Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is a {myOtherGame.genre} game!");
         }
     }
--- Constructors_Mcvey_Zander/game.cs
+++ Constructors_Mcvey_Zander/game.cs
@@ -6,14 +6,14 @@ namespace Constructors_Mcvey_Zander
 {
     class game
     {
-        //
+        //This list out the public string of what the title of the game is and what genre it is.
         public string title, genre;
 
-        //
+        //Lists out theOtherGame's title and genre and saves them as strings.
         public game(string titleParam = "Unknown Title", string genreParam = "Casual")
         {
 
-            //
+            //Connects titleParam to the word "title" and genreParam to the world "genre".
             title = titleParam;
             genre = genreParam;
 
