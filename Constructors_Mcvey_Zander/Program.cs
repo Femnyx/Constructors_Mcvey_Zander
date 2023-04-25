55123b3141b7aa5d68510cab52ab038622cd7eea
Zander <114107976+Femnyx@users.noreply.github.com>
2023-04-25 14:24:16 -0500

    Did the things!

--- Constructors_Mcvey_Zander/Program.cs
+++ Constructors_Mcvey_Zander/Program.cs
@@ -6,7 +6,17 @@ namespace Constructors_Mcvey_Zander
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //
+            game myGame = new game("Legend of Zelda: WindWaker", "Adventure");
+            
+            //
+            Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre} game!");
+
+            //
+            game myOtherGame = new game();
+
+            //
+            Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is a {myOtherGame.genre} game!");
         }
     }
 }
---
+++ Constructors_Mcvey_Zander/game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors_Mcvey_Zander
+{
+    class game
+    {
+        //
+        public string title, genre;
+
+        //
+        public game(string titleParam = "Unknown Title", string genreParam = "Casual")
+        {
+
+            //
+            title = titleParam;
+            genre = genreParam;
+
+        }
+
+        
+    }
+}
